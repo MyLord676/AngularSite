d836d6fe5669e4cddf819162710c8974e0b6e52a
Aleksey <aleksey18rus@inbox.ru>
2023-03-29 16:25:03 +0400

    footer

--- my-app/src/app/career/career.component.ts
+++ my-app/src/app/career/career.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-career',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./career.component.css']
 })
 export class CareerComponent {
+  public screenWidth: number;
+  public screenHeight: number;
 
+  constructor() {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
 }
