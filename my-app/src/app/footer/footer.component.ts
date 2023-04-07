cf86f496343193a01ef4040e55634a1b5ef2992b
Aleksey <aleksey18rus@inbox.ru>
2023-04-07 16:56:44 +0400

    footer back

--- my-app/src/app/footer/footer.component.ts
+++ my-app/src/app/footer/footer.component.ts
@@ -13,22 +13,47 @@ export class FooterComponent {
   public wrapperWidth: number;
   public wrapperHeight: number;
 
+  public imagePosition: string;
+  public imageUrl: string;
+
   constructor() {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
 
     this.wrapperWidth = window.innerWidth;
     this.wrapperHeight = window.innerHeight;
+
+    this.imageUrl = this.calculateImageUrl();
+    this.imagePosition = this.calculateImagePosition();
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
+
+    this.imageUrl = this.calculateImageUrl();
+    this.imagePosition = this.calculateImagePosition();
   }
 
   onResized(event: ResizedEvent) {
     this.wrapperWidth = event.newRect.height;
     this.wrapperHeight = event.newRect.height;
+  }
+
+  calculateImagePosition() {
+    if (this.screenWidth <= 800 && this.screenWidth > 480)
+      return 'right 40px';
+    if (this.screenWidth <= 480)
+      return 'right 120px';
+    return (this.screenWidth / 2).toString() + 'px' + ' top';
+  }
+
+  calculateImageUrl() {
+    if (this.screenWidth <= 800 && this.screenWidth > 480)
+      return 'url(/assets/images/footer1.png)';
+    if (this.screenWidth <= 480)
+      return 'url(/assets/images/footer2.png)';
+    return 'url(/assets/images/footer.png)';
   }
 }
