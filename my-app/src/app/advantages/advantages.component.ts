d382255bf9cb9b59a30496f4dcc5217c6c5bca60
Aleksey <aleksey18rus@inbox.ru>
2023-04-07 17:42:36 +0400

    fixes

--- my-app/src/app/advantages/advantages.component.ts
+++ my-app/src/app/advantages/advantages.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { ResizedEvent } from 'angular-resize-event';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-advantages',
@@ -8,31 +7,55 @@ import { ResizedEvent } from 'angular-resize-event';
 })
 
 export class AdvantagesComponent {
-  @ViewChild("image")image!: ElementRef<HTMLDivElement>;
+  @ViewChild("image") image!: ElementRef<HTMLDivElement>;
+  @ViewChild("content") content!: ElementRef<HTMLDivElement>;
+
   zero = 0;
   bottom = "no-repeat";
   public screenWidth: number;
   public screenHeight: number;
 
-  public wrapperWidth: number;
-  public wrapperHeight: number;
+  public wrapperWidth: number = 0;
+  public wrapperHeight: number = 0;
 
-  constructor() {
+  public imagePosition: string = '';
+  public imageUrl: string = '';
+
+  constructor(private cdRef: ChangeDetectorRef) {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
-
-    this.wrapperWidth = window.innerWidth;
-    this.wrapperHeight = window.innerHeight;
   }
-  
+
+  ngAfterViewInit(): void {
+    this.wrapperWidth = this.content.nativeElement.offsetWidth;
+    this.wrapperHeight = this.content.nativeElement.offsetHeight;
+    this.cdRef.detectChanges();
+
+    this.imageUrl = this.calculateImageUrl();
+    this.imagePosition = this.calculateImagePosition();
+  }
+
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
+
+    this.wrapperWidth = this.content.nativeElement.offsetWidth;
+    this.wrapperHeight = this.content.nativeElement.offsetHeight;
+
+    this.imageUrl = this.calculateImageUrl();
+    this.imagePosition = this.calculateImagePosition();
   }
 
-  onResized(event: ResizedEvent) {
-    this.wrapperWidth = event.newRect.width;
-    this.wrapperHeight = event.newRect.height;
+  calculateImagePosition() {
+    return (this.screenWidth / 2 - this.wrapperWidth / 2 - 12).toString() + 'px' + ' bottom';
+  }
+
+  calculateImageUrl() {
+    if (this.screenWidth <= 1450 && this.screenWidth > 480)
+      return 'url(/assets/images/advantages1.png)';
+    if (this.screenWidth <= 480)
+      return 'url(/assets/images/advantages2.png)';
+    return 'url(/assets/images/advantages.png)';
   }
 }
