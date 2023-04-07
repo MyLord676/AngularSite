32e6e124209499eb7f5338b4aaee007c611ee797
Aleksey <aleksey18rus@inbox.ru>
2023-04-07 16:29:02 +0400

    fixes and header back

--- my-app/src/app/header/header.component.ts
+++ my-app/src/app/header/header.component.ts
@@ -10,7 +10,9 @@ export class HeaderComponent {
   public dropDown: boolean;
   public screenWidth: number;
   public screenHeight: number;
-  @ViewChild("main")main!: ElementRef<HTMLDivElement>;
+  public imagePosition: string;
+  public imageUrl: string;
+  @ViewChild("main") main!: ElementRef<HTMLDivElement>;
 
   /*@Output() scrollToEvent = new EventEmitter<Event>();*/
 
@@ -18,6 +20,8 @@ export class HeaderComponent {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
     this.dropDown = false;
+    this.imageUrl = this.calculateImageUrl();
+    this.imagePosition = this.calculateImagePosition();
     /*this.main.nativeElement.clientHeight*/
   }
 
@@ -27,15 +31,30 @@ export class HeaderComponent {
     this.screenHeight = window.innerHeight;
     if (this.screenWidth > 1100)
       this.dropDown = false;
+
+    this.imageUrl = this.calculateImageUrl();
+    this.imagePosition = this.calculateImagePosition();
   }
 
   dropdownClick(event: Event) {
     this.dropDown = !this.dropDown;
-    if (this.screenWidth > 1100)
-      this.dropDown = false;
   }
 
   scrollTo(event: Event) {
     document.getElementsByTagName((event.target as HTMLDivElement).id)[0].scrollIntoView();
+  }
+
+  calculateImagePosition() {
+    if (this.screenWidth <= 800)
+      return 'right';
+    return (this.screenWidth / 2 - 161).toString() + 'px';
+  }
+
+  calculateImageUrl() {
+    if (this.screenWidth <= 800 && this.screenWidth > 480)
+      return 'url(/assets/images/header-image1.png)';
+    if (this.screenWidth <= 480)
+      return 'url(/assets/images/header-image2.png)';
+    return 'url(/assets/images/header-image.png)';
   }
 }
